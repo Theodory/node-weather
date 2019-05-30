b188df0ae2ba66df6fac5d82b2fea68d609faf1d
Theodory <theodoryf@gmail.com>
2019-05-30 18:23:39 +0300

    use yargs to filter on the commandline tool

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const request = require('request')
+const argv = require('yargs').argv
 const http = require('http')
 
 let apiKey = 'bead33159d7b031b258326b1543c13a6';
-let city = 'Dar es salaam';
+let city = argv.anotherCity ||'Dar es salaam';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 request.get(url,(err,response,body)=>{
