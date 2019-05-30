8ffb68a1f5bf97c1c3da84686714ed2d021caeb8
Theodory <theodoryf@gmail.com>
2019-05-30 19:02:01 +0300

    Add style

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const argv = require('yargs').argv
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 const port = 3000
 
@@ -10,18 +11,8 @@ let apiKey = 'bead33159d7b031b258326b1543c13a6';
 let city = argv.anotherCity ||'Dar es salaam';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
-// request.get('/',(err,response,body)=>{
-// 	if(err){
-// 		console.log(error);
-// 	}else{
-// 		let data = JSON.parse(body);
-// 		let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
-// 		console.log(textMessage);
-// 	}
-// })
-
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 })
 
 app.listen(port, function () {
